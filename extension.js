922d43484407c78167df22d12c4aa18bddf0ef50
tct123 <42028373+tct123@users.noreply.github.com>
2024-05-03 15:13:39 +0200

    .

--- extension.js
+++ extension.js
@@ -12,7 +12,7 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscodetest" is now active!');
+	console.log('Hello world by tct123');
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
